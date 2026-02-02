79a74c36402cab8ed9f994360344f8e8711287bb
Nobori77 <minginew77@gmail.com>
2026-02-02 17:50:32 +0900

    feat:day03

--- study/src/App.js
+++ study/src/App.js
@@ -1,31 +1,9 @@
-import CompanyContainer from "./map/basic/CompanyContainer";
-import FoodContainer from "./map/basic/FoodContainer";
-import SportContainer from "./map/basic/SportsContainer";
-import CheckboxContainer from "./map/expert/CheckboxContainer";
-import ProductContainer from "./map/expert/ProductContainer";
-import AttendanceContainer from "./map/normal/AttendanceContainer";
-import Color from "./state/Color";
-import Count from "./state/Count";
-import Hobby from "./state/Hobby";
-import Name from "./state/Name";
-import Person from "./state/Person";
-import Sound from "./state/Sound";
+import FoodContainer from "./ref/expert/FoodContainer";
 
 function App() {
   return (
     <>
-      <Count />
-      <Sound />
-      <Name />
-      <Color />
-      <Hobby />
-      <Person />
-      <FoodContainer />
-      <CompanyContainer />
-      <SportContainer />
-      <AttendanceContainer />
-      <ProductContainer />
-      <CheckboxContainer />
+    <FoodContainer />
     </>
   );
 }
